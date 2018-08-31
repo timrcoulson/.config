1704a3627f143c3b335fa47e86046faeb0c95e4b
Tim Coulson <tim.r.coulson@gmail.com>
2018-08-31 14:14:01 +0100

    Brewfile

---
+++ Brewfile
@@ -0,0 +1,30 @@
+# Languages
+brew "go"
+brew "node"
+
+# IDEs
+cask "phpstorm"
+cask "intellij-idea"
+cask "goland"
+
+# Tooling
+brew "terraform"
+cask "sequel-pro"
+brew "gpg"
+cask "gpg-suite"
+
+# Productivity
+cask "slack"
+cask "google-chrome"
+cask "1password"
+cask "iterm2"
+cask "atom"
+
+# Containers
+cask "docker"
+brew "kubectl"
+brew "docker-compose"
+
+# Fun
+cask "spotify"
+cask "ableton-live-standard"
--- dock.sh
+++ dock.sh
@@ -1,11 +1,23 @@
+#!/usr/bin/env bash
+
 defaults write com.apple.dock persistent-apps -array
-APPS=( Ableton\ Live\ 10\ Standard.app Slack.app Google\ Chrome.app 1Password\ 7.app GoLand.app IntelliJ\ IDEA.app iTerm.app Spotify.app Notes.app Sequel\ Pro.app PhpStorm.app)
+APPS=(
+  Ableton\ Live\ 10\ Standard
+  Slack
+  Google\ Chrome
+  1Password\ 7
+  GoLand
+  IntelliJ\ IDEA
+  iTerm
+  Spotify
+  Notes
+  Sequel\ Pro
+  PhpStorm
+)
 
 for i in "${APPS[@]}"
 do
   echo "Adding $i to dock"
-  defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$i</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+  defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$i.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 done
 killall Dock;
-
-
